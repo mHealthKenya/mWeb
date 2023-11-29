3001f906cd108350352135f030789c09e026b0d9
Mutwoki Dennis Mureti <dennis.mutwoki@strathmore.edu>
2023-11-29 23:18:23 +0300

    dash changed to danger color

--- src/components/Chv/Dashboard/Dashboard.tsx
+++ src/components/Chv/Dashboard/Dashboard.tsx
@@ -43,40 +43,11 @@ const CHVHome: FC<{
   const currentDate = new Date()
   const currentMonth = months[currentDate.getMonth()]
 
-  //   const currencyCode = 'KSH'
-  //   const locale = 'en-KE'
-
-  //   const currencyFormatter = new Intl.NumberFormat(locale, {
-  //     style: 'currency',
-  //     currency: currencyCode
-  //   })
-
-  //   let extended_list_of_strings: string[] = []
-
-  // const list_of_strings: string[] = ['success', 'info', 'warning', 'danger', 'primary']
-
-  //   if (visits_distribution.length > list_of_strings.length) {
-  //     const repeat_times: number = Math.floor(visits_distribution.length / list_of_strings.length)
-
-  //     for (let i = 0; i < repeat_times; i++) {
-  //       extended_list_of_strings = extended_list_of_strings.concat(list_of_strings)
-  //     }
-
-  //     const remaining_numbers: number = visits_distribution.length % list_of_strings.length
-  //     if (remaining_numbers > 0) {
-  //       extended_list_of_strings = extended_list_of_strings.concat(
-  //         list_of_strings.slice(0, remaining_numbers)
-  //       )
-  //     }
-  //   } else {
-  //     extended_list_of_strings = list_of_strings.slice(0, visits_distribution.length)
-  //   }
-
   return (
     <>
       <div className="row">
         <div className="col-sm-6 col-lg-3">
-          <Card bg="primary" text="white" className="mb-4">
+          <Card bg="danger" text="white" className="mb-4">
             <Card.Body className="pb-0 d-flex justify-content-between align-items-start">
               <div>
                 <Box sx={{ m: 2 }}>
@@ -109,64 +80,6 @@ const CHVHome: FC<{
             </Card.Body>
           </Card>
         </div>
-        {/* <div className="col-sm-6 col-lg-3">
-          <Card bg="danger" text="white" className="mb-4">
-            <Card.Body className="pb-0 d-flex justify-content-between align-items-start">
-              <Box sx={{ m: 2 }}>
-                <div className="fs-4 fw-semibold">{currencyFormatter.format(total_sms_cost)}</div>
-                <div>Total SMS Cost</div>
-              </Box>
-            </Card.Body>
-          </Card>
-        </div> */}
-
-        {/* <div className="col-sm-6 col-lg-3">
-          <Card bg="secondary" text="white" className="mb-4">
-            <Card.Body className="pb-0 d-flex justify-content-between align-items-start">
-              <div>
-                <Box sx={{ m: 2 }}>
-                  <div className="fs-4 fw-semibold">{total_sms_count.count.toLocaleString()}</div>
-                  <div>Total SMS Count</div>
-                </Box>
-              </div>
-            </Card.Body>
-          </Card>
-        </div> */}
-
-        {/* <div className="col-sm-6 col-lg-3">
-          <Card bg="info" text="white" className="mb-4">
-            <Card.Body className="pb-0 d-flex justify-content-between align-items-start">
-              <Box sx={{ m: 2 }}>
-                <div className="fs-4 fw-semibold">{monthly_sms_count.count.toLocaleString()}</div>
-                <div>{currentMonth} SMS Count</div>
-              </Box>
-            </Card.Body>
-          </Card>
-        </div> */}
-
-        {/* <div className="col-sm-6 col-lg-3">
-          <Card bg="danger" text="white" className="mb-4">
-            <Card.Body className="pb-0 d-flex justify-content-between align-items-start">
-              <Box sx={{ m: 2 }}>
-                <div className="fs-4 fw-semibold">{currencyFormatter.format(monthly_sms_cost)}</div>
-                <div>{currentMonth} SMS Cost</div>
-              </Box>
-            </Card.Body>
-          </Card>
-        </div> */}
-        {/* 
-        <div className="col-sm-6 col-lg-3">
-          <Card bg="primary" text="white" className="mb-4">
-            <Card.Body className="pb-0 d-flex justify-content-between align-items-start">
-              <Box sx={{ m: 2 }}>
-                <div className="fs-4 fw-semibold">
-                  {monthly_clinic_visits.count.toLocaleString()}
-                </div>
-                <div>{currentMonth} Clinic Visits</div>
-              </Box>
-            </Card.Body>
-          </Card>
-        </div> */}
       </div>
 
       <Card className="mb-4">
